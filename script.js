3ed6e69d43d24f99ba6b70134f3af8d0804bae0a
dvs-codes <123456789+odin@users.noreply.github.com>
2024-01-20 07:11:18 +0530

    complete revamp of the procedure of how a card is added to display.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary =[]
-let newBook
+const mainBody = document.querySelector('body')
 const formAuthor = document.querySelector("#author") 
 const formTitle = document.querySelector('#title')
 const formPages = document.querySelector('#number_of_pages')
@@ -24,76 +24,87 @@ function Book(author, title, pages, readStatus) {
 }
 
 Book.prototype.changeReadStatus = function() {
-
+     if (this.readStatus === 'read') {
+        this.readStatus = 'not read'
+     } else  {
+        this.readStatus = 'read'
+     }
 }
-
-
 
 function addBookToLibrary() {
     //This function adds a book object into the array of library
     const newBook =new Book(formAuthor.value, formTitle.value, 
-        formPages.value, formStatus.value);
+    formPages.value, formStatus.value);
     myLibrary.push(newBook)
     console.log(newBook)
-    newBook.changeReadStatus()
+
+
+    myLibrary.forEach((book) => {
+        console.log("works")
+        // creates a card from the info gathered in myLibrary array
+        //main card
+        let infoCard = document.createElement('div')
+        infoCard.classList.add(`card`)
+        infoCard.classList.add(`${myLibrary.length}`)
+        booksDisplay.appendChild(infoCard)
+    
+        //author title line
+        let authorCard = document.createElement('div')
+        authorCard.classList.add('author-name')
+        authorCard.innerText = `Author: ${book.author}`
+        infoCard.appendChild(authorCard)
+        
+        //bookname 
+        let titleCard = document.createElement('div')
+        titleCard.classList.add('book-name')
+        titleCard.innerText = `Book: ${book.title}`
+        infoCard.appendChild(titleCard)
+
+        //page numbers
+        let pageNumCard =  document.createElement('div')
+        pageNumCard.classList.add('page-num')
+        pageNumCard.innerText =`Number of pages: ${book.pages}`
+        infoCard.appendChild(pageNumCard)
+
+        //read status
+        let bookStatusCard = document.createElement('button')
+        bookStatusCard.classList.add('status')
+        bookStatusCard.innerText = `${book.readStatus}`
+        infoCard.appendChild(bookStatusCard)
+
+        //clicking read button changes status of book read
+        bookStatusCard.addEventListener('click', () => {
+            book.changeReadStatus()
+            bookStatusCard.innerText = `${book.readStatus}`
+            console.log(myLibrary)
+        })
+
+        //remove button to remove specific card
+        let removeButton = document.createElement('button')
+        removeButton.classList.add('remove')
+        removeButton.innerText = 'remove'
+        infoCard.appendChild(removeButton)
+})
 }
 
 confirmButton.addEventListener('click', (event) => {
     event.preventDefault()
-    addBookToLibrary()
-    cardAdder()
-    document.querySelector(".book-details-form").reset()
-    bookDialog.close()
 
-})
-
-function cardAdder() { 
-    // creates a card from the info gathered in myLibrary array
-    //main card
-    let infoCard = document.createElement('div')
-    infoCard.classList.add(`card`)
-    infoCard.classList.add(`${myLibrary.length}`)
-    booksDisplay.appendChild(infoCard)
-
-    //author title line
-    let authorCard = document.createElement('div')
-    authorCard.classList.add('author-name')
-    authorCard.innerText = `Author: ${myLibrary[myLibrary.length-1].author}`
-    infoCard.appendChild(authorCard)
-
-    //bookname 
-    let titleCard = document.createElement('div')
-    titleCard.classList.add('book-name')
-    titleCard.innerText = `Book: ${myLibrary[myLibrary.length-1].title}`
-    infoCard.appendChild(titleCard)
-
-    //page numbers
-    let pageNumCard =  document.createElement('div')
-    pageNumCard.classList.add('page-num')
-    pageNumCard.innerText =`Number of pages: ${myLibrary[myLibrary.length-1].pages}`
-    infoCard.appendChild(pageNumCard)
-
-    //read status
-    let bookStatusCard = document.createElement('button')
-    bookStatusCard.classList.add('status')
-    bookStatusCard.innerText = `${myLibrary[myLibrary.length-1].readStatus}`
-    infoCard.appendChild(bookStatusCard)
-   
-
-    //adds a remove button
-    let removeButton = document.createElement('button')
-    // removeButton.classList.add('remove')
-    removeButton.classList.add(`${myLibrary.length}`)
-    removeButton.innerText = "Remove"
-    infoCard.appendChild(removeButton)
-
-    removeButton.addEventListener('click', () => {
-        
-        element = document.querySelector(`.card`)
-        element.remove()
+    //refreshes and deletes already created book card so the loop 
+    //can go through each of them again.
+    myLibrary.forEach(() =>{
+        booksDisplay.removeChild(booksDisplay.firstElementChild)
     })
 
-}
+    addBookToLibrary()
+    document.querySelector(".book-details-form").reset()
+    bookDialog.close()
+    console.log(myLibrary)
+})
+
+
+
+
 
 
 
